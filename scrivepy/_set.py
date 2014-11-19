6a728137fcd1f4d6f4a738037a50a03476ad3f86
Bartek Ćwikłowski <paczesiowa@gmail.com>
2014-11-19 19:34:16 +0100

    add difference_update method to ScriveSet

--- scrivepy/_set.py
+++ scrivepy/_set.py
@@ -15,29 +15,64 @@ class ScriveSet(set, _object.ScriveObject):
         self._check_getter()
         result = set.copy(self)
         result._read_only = self._read_only
+        result._invalid = False
         return result
 
     def difference_update(self, iterable=()):
         self._check_setter()
         return set.difference_update(self, iterable)
 
-# x.add                          x.copy                         x.difference_update            x.intersection                 x.isdisjoint                   x.issuperset                   x.remove                       x.symmetric_difference_update  x.update
-# x.clear                        x.difference                   x.discard                      x.intersection_update          x.issubset                     x.pop                          x.symmetric_difference         x.union
 
-
-# x.__and__           x.__contains__      x.__eq__            x.__getattribute__  x.__iand__          x.__isub__          x.__le__            x.__ne__            x.__rand__          x.__repr__          x.__rxor__          x.__str__           x.__xor__
-# x.__class__         x.__delattr__       x.__format__        x.__gt__            x.__init__          x.__iter__          x.__len__           x.__new__           x.__reduce__        x.__ror__           x.__setattr__       x.__sub__
-# x.__cmp__           x.__doc__           x.__ge__            x.__hash__          x.__ior__           x.__ixor__          x.__lt__            x.__or__            x.__reduce_ex__     x.__rsub__          x.__sizeof__        x.__subclasshook__
- 
-# s = ScriveSet()
-# s._read_only = True
-# print s, type(s), s._read_only
-# s2 = s.copy()
-# print s2, type(s2), s2._read_only
-
-
-# x = ScriveSet([1,2,3])
-# y = ScriveSet([2,3])
-# print x.difference_update()
-# print x, y
-
+# 4/57
+# intersection
+# isdisjoint
+# issuperset
+# remove
+# symmetric_difference_update
+# update
+# clear
+# difference
+# discard
+# intersection_update
+# issubset
+# pop
+# symmetric_difference
+# union
+# __and__
+# __contains__
+# __eq__
+# __getattribute__
+# __iand__
+# __isub__
+# __le__
+# __ne__
+# __rand__
+# __repr__
+# __rxor__
+# __str__
+# __xor__
+# __class__
+# __delattr__
+# __format__
+# __gt__
+# __iter__
+# __len__
+# __new__
+# __reduce__
+# __ror__
+# __setattr__
+# __sub__
+# __cmp__
+# __doc__
+# __ge__
+# __hash__
+# __ior__
+# __ixor__
+# __lt__
+# __or__
+# __reduce_ex__
+# __rsub__
+# __sizeof__
+# __subclasshook__
+# _set_read_only
+# _set_invalid
--- tests/test_set.py
+++ tests/test_set.py
@@ -53,3 +53,72 @@ class ScriveSetTest(utils.TestCase):
         s._set_invalid()
         with self.assertRaises(INV):
             s.add(3)
+
+    def test_copy(self):
+        o1 = _object.ScriveObject()
+        o2 = _object.ScriveObject()
+        o3 = _object.ScriveObject()
+
+        s1 = S([o1])
+        self.assertEqual(1, len(s1))
+        self.assertTrue(o1 in s1)
+        self.assertFalse(o2 in s1)
+        self.assertFalse(o3 in s1)
+
+        s2 = s1.copy()
+        self.assertTrue(isinstance(s2, S))
+
+        self.assertTrue(o1 in s2)
+        self.assertFalse(o2 in s2)
+        self.assertFalse(o3 in s2)
+
+        s1.add(o2)
+
+        self.assertTrue(o1 in s1)
+        self.assertTrue(o2 in s1)
+        self.assertFalse(o3 in s1)
+
+        self.assertTrue(o1 in s2)
+        self.assertFalse(o2 in s2)
+        self.assertFalse(o3 in s2)
+
+        s2.add(o3)
+
+        self.assertTrue(o1 in s1)
+        self.assertTrue(o2 in s1)
+        self.assertFalse(o3 in s1)
+
+        self.assertTrue(o1 in s2)
+        self.assertFalse(o2 in s2)
+        self.assertTrue(o3 in s2)
+
+        s1._set_read_only()
+        self.assertTrue(s1._read_only)
+        self.assertFalse(s2._read_only)
+
+        s2._set_invalid()
+        self.assertFalse(s1._invalid)
+        self.assertTrue(s2._invalid)
+
+        s = S()
+        s._set_read_only()
+        s.copy()
+        s._set_invalid()
+        with self.assertRaises(INV):
+            s.copy()
+
+    def test_difference_update(self):
+        s = S([1, 2, 3])
+        s.difference_update([2, 4])
+        self.assertEqual(2, len(s))
+        self.assertTrue(1 in s)
+        self.assertFalse(2 in s)
+        self.assertTrue(3 in s)
+        self.assertFalse(4 in s)
+
+        s._set_read_only()
+        with self.assertRaises(RO):
+            s.difference_update([])
+        s._set_invalid()
+        with self.assertRaises(INV):
+            s.difference_update([])
